77fa058dd484bde167c0bc43baf3c80b90bc65af
Spencer Hum <83096779+spencerhum04@users.noreply.github.com>
2026-03-16 10:34:05 -0400

    Remove email enqueueing logic from main function

--- main.go
+++ main.go
@@ -12,13 +12,5 @@ func main() {
 	database := db.Connect()
 	defer database.Close()
 
-	job, err := queue.Enqueue(database, "send email", map[string]any{
-		"to":      "test@example.com",
-		"subject": "test message from DuckMQ",
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("Enqueued job id=%d type=%s:", job.ID, job.Type)
+	worker.Start(database)
 }
